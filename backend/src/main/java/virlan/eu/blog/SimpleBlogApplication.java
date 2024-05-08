923670ae725f14ad6e43c7c31d620fead88a9d7d
Vladimir <acv4acc@gmail.com>
2024-05-08 22:33:06 +0100

    First commit

---
+++ backend/src/main/java/virlan/eu/blog/SimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package virlan.eu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/virlan/eu/blog/SimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package virlan.eu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
